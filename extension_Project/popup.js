62de31c3f74d8c9a91c213035fb8a58a360617d2
Tom Zhang <hothalftom@gmail.com>
2022-01-26 13:23:46 -0500

    change

--- extension_Project/popup.js
+++ extension_Project/popup.js
@@ -10,6 +10,33 @@ window.addEventListener('load', (event) => {
         
 
 
-    })
+    });
+    
+    document.getElementById("Change-Font").addEventListener("change", event =>{
+        changeConfig("Change-Font", event.target.checked);
+    });//Changes Font
+    document.getElementById("Change-Colour").addEventListener("change", event =>{
+        changeConfig("Change-Colour", event.target.checked);
+    });//Changes Box Colour
+
+
+    function restoreDefaultConfig(){
+        const defaultConfiguration = {
+            "configuration":{
+
+                "Change_Font": false,
+            }
+        }
+
+        sendToProgramJS(defaultConfiguration);
+
+        setUI(defaultConfiguration["configuration"]);
+
+        chrome.storage.sync.set({'configuration': defaultConfiguration["configuration"]}, function(){});
+    }
+
+
+}); //CLOSES WINDOW EVENT LISTENER
+
 
     
