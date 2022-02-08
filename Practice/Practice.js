b136f028b1213a065c237d532a6f53d75b8404f6
Tom Zhang <hothalftom@gmail.com>
2022-02-08 11:01:55 -0500

    s

--- Practice/Practice.js
+++ Practice/Practice.js
@@ -1,42 +1,20 @@
-let notes = [ 60, 62, 64, 65, 67, 69, 71];
-
-let osc;
-
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(800, 800);
+  smooth(); 
+}
+
+function draw() {
+  background(253);
+  stroke(0); 
+  noFill();
   
-  // A triangle oscillator
-  osc = new p5.TriOsc();
-  // Start silent
-  osc.start();
-  osc.amp(0);
-}
-
-// A function to play a note
-function playNote(note, duration) {
-  osc.freq(midiToFreq(note));
-  // Fade it in
-  osc.fade(0.5,0.2);
-
-  // If we sest a duration, fade it out
-  if (duration) {
-    setTimeout(function() {
-      osc.fade(0,0.2);
-    }, duration-50);
+  var constantFactor = 1.315;
+  var circleSize = 20; 
+  
+  //draws 20 concentric circles of decreasing diameter and decreasing lineWeight
+  for (var i=1; i<20; i++) {
+    strokeWeight(circleSize/25.0); 
+    ellipse(width/2,height, circleSize, circleSize);
+    circleSize = circleSize * constantFactor; 
   }
-}
-
-
-// When we click
-function mousePressed(event) {
-  if(event.button == 0 && event.clientX < width && event.clientY < height) {
-    // Map mouse to the key index
-    let key = floor(map(mouseX, 0, width, 0, notes.length));
-    playNote(notes[key]);
-  }
-}
-
-// Fade it out when we release
-function mouseReleased() {
-  osc.fade(0,0.5);
 }
