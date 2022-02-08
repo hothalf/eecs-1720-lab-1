c7103571776a8cf2fd5daeabe52b51ac75dbf443
Tom Zhang <hothalftom@gmail.com>
2022-02-07 19:55:52 -0500

    a

---
+++ Practice/Practice.js
@@ -0,0 +1,22 @@
+
+var p,c,d,e,f,t;
+function setup(){
+
+    createCanvas(800,800);
+    smooth();
+}
+
+function draw(){
+    background(253);
+    stroke(0);
+    noFill();
+    var constantFactor = 1.315;
+    var circleSize = 20; 
+    
+    //draws 20 concentric circles of decreasing diameter and decreasing lineWeight
+    for (var i=1; i<20; i++) {
+      strokeWeight(circleSize/25.0); 
+      ellipse(width/2,height, circleSize, circleSize);
+      circleSize = circleSize * constantFactor; 
+    }
+}
